0eab4ab64b6b8886155725984cdb52dea324b286
matthova <matthova87@gmail.com>
2016-05-25 10:56:41 -0700

    updated script

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Follow GPIO resources [here](https://github.com/matthova/blink)
+Follow GPIO resources [here](http://makezine.com/projects/tutorial-raspberry-pi-gpio-pins-and-python/)
--- button.py
+++ button.py
@@ -1,14 +1,21 @@
 import RPi.GPIO as GPIO # Import GPIO library
 import time
 
-prev_input = 0
-while True:
-  #take a reading
-  input = GPIO.input(17)
-  #if the last reading was low and this one high, print
-  if ((not prev_input) and input):
-    print("Button pressed")
-  #update previous input
-  prev_input = input
-  #slight pause to debounce
-  time.sleep(0.05)
+try:
+    inputPin = 23
+    GPIO.setmode(GPIO.BOARD)     # Use board pin numbering
+    GPIO.setup(inputPin, GPIO.OUT) # Setup GPIO Pin 7 to OUT	
+    
+    prev_input = 0
+    while True:
+      #take a reading
+      input = GPIO.input(inputPin)
+      #if the last reading was low and this one high, print
+      if ((not prev_input) and input):
+        print("Button pressed")
+      #update previous input
+      prev_input = input
+      #slight pause to debounce
+      time.sleep(0.05)
+except KeyboardInterrupt:
+    GPIO.cleanup()               # Clean up the hardware drivers
